760dfe097344698a99c1d0b4bda5452c54afdcbb
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-29 01:45:06 +0530

    add read values

--- src/app/addnews/addnews.component.ts
+++ src/app/addnews/addnews.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AddnewsComponent {
 
+  id=""
+  name=""
+  title=""
+  description=""
+  url=""
+  urlimage=""
+  publishedat=""
+  content=""
+
+  readValues=()=>
+  {
+    let data:any=
+    {
+      "id":this.id,
+      "name":this.name,
+      "title":this.title,
+      "description":this.description,
+      "url":this.url,
+      "urlimage":this.urlimage,
+      "publishedat":this.publishedat,
+      "content":this.content
+    }
+    console.log(data)
+  }
+
+
+
 }
