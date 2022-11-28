74a10e9a2565ef80ff0f989d65d3c95132412ea3
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-29 01:01:24 +0530

    add viewnews

---
+++ src/app/viewnews/viewnews.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewnews',
+  templateUrl: './viewnews.component.html',
+  styleUrls: ['./viewnews.component.css']
+})
+export class ViewnewsComponent {
+
+}
