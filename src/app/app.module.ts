7b8a86220b8115205569892f8ca820bcbd738637
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-29 00:48:32 +0530

    add news entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddnewsComponent } from './addnews/addnews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddnewsComponent
   ],
   imports: [
     BrowserModule,
